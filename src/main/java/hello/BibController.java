3b2894cc9c068f046cdaa05cfb83b8a1e0df96b8
anthony <a.canicatti@gmail.com>
2017-03-26 18:40:41 -0400

    First commit

---
+++ src/main/java/hello/BibController.java
@@ -0,0 +1,68 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class BibController {
+
+	// INSERT query
+	String INSERT = "INSERT INTO bibentries (title, author, year, journal)" +
+			" VALUES (?,?,?,?)";
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@GetMapping("/biblio")
+	public String biblioForm(Model model) {
+
+		// first handle database creation if not already done
+		jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS bibentries (id SERIAL, title VARCHAR(255), " +
+				"author VARCHAR(255), year INTEGER, journal VARCHAR(255))");
+
+		// now retrieve everything in the database
+		List<Map<String, Object>> results = jdbcTemplate.queryForList("SELECT title," +
+				" author, year, journal from bibentries");
+
+		ArrayList<BibEntry> bibEntries = new ArrayList<BibEntry>();
+		for(Map<String, Object> map : results){
+			String author = (String) map.get("author");
+			String title = (String) map.get("title");
+			int year = Integer.parseInt(map.get("year").toString());
+			String journal = (String) map.get("journal");
+
+			// add the entry to the list
+			BibEntry entry = new BibEntry();
+			entry.setAuthor(author);
+			entry.setTitle(title);
+			entry.setYear(year);
+			entry.setJournal(journal);
+			bibEntries.add(entry);
+		}
+
+		model.addAttribute("entry", new BibEntry());
+		model.addAttribute("entries", bibEntries);
+		return "biblio";
+	}
+
+	@PostMapping("/biblio")
+	public String biblioSubmit(@ModelAttribute BibEntry entry, Model model){
+		model.addAttribute("entry", entry);
+		//bibEntries.add(entry);
+		String author = entry.getAuthor();
+		String title = entry.getTitle();
+		int year = entry.getYear();
+		String journal = entry.getJournal();
+
+		jdbcTemplate.update(INSERT, title, author, year, journal);
+		return "addEntry";
+	}
+
+}
---
+++ src/main/java/hello/BibEntry.java
@@ -0,0 +1,43 @@
+package hello;
+
+/**
+ * Created by anthony on 3/26/17.
+ */
+public class BibEntry {
+
+    private String author, title, journal;
+    private int year;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getJournal() {
+        return journal;
+    }
+
+    public void setJournal(String journal) {
+        this.journal = journal;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+}
